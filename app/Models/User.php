d0473fab849ad037a2a96020100bde87e77cdd01
Niels Veenstra <nielsveenstra@lfh.nl>
2026-01-27 11:52:49 +0100

    added orgination migations, model and seeders

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
@@ -45,5 +46,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+
+    public function Organisation(): belongsTo
+    {
+        $this->belongsTo(Organisation::class);
     }
 }
